72c264b4a854f53b7472f9958cb3638d5e231024
xappuipi <134952022+xappuipi@users.noreply.github.com>
2024-07-15 17:21:06 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // Pozwala na parsowanie JSON
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+const PytanieSchema = new mongoose.Schema({
+    pytanie: String,
+    odpowiedz: String,
+  });
+  
+const Pytanie = mongoose.model('Pytanie', PytanieSchema);
+
+// app.post('/users', async (req, res) => {
+//     const user = new User(req.body);
+//     try {
+//         await user.save();
+//       res.status(201).send(user);
+//     } catch (error) {
+//       res.status(400).send(error);
+//     }
+//   });
+  
+app.get('/pytania', async (req, res) => {
+    try {
+      const pytania = await Pytanie.find();
+      res.status(200).send(users);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
